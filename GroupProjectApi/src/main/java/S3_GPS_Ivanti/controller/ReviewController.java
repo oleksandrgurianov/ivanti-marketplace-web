cd97877abf4d2c8fc569001d3f52e6c212251c16
Rodriguez Morales,Noelia N <n.rodriguezmorales@student.fontys.nl>
2022-04-01 16:25:01 +0200

    project startup :)

--- GroupProjectApi/src/main/java/S3_GPS_Ivanti/controller/ReviewController.java
+++ GroupProjectApi/src/main/java/S3_GPS_Ivanti/controller/ReviewController.java
@@ -4,13 +4,6 @@ import S3_GPS_Ivanti.business.ReviewService;
 import S3_GPS_Ivanti.business.UserService;
 import S3_GPS_Ivanti.model.Review;
 import S3_GPS_Ivanti.model.User;
-<<<<<<< HEAD
-=======
-=======
-import com.example.S3_GPS_Ivanti.business.ReviewService;
-import com.example.S3_GPS_Ivanti.model.*;
->>>>>>> 75208ff8c276fbe9838fb7b8e36d120aec7926ff:GroupProjectApi/src/main/java/com/example/S3_GPS_Ivanti/controller/ReviewController.java
->>>>>>> fc0265dccf440bbb352a970c604edee58f2bb6dc
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -55,13 +48,9 @@ public class ReviewController {
     public ResponseEntity updateReview(@RequestBody Review review) {
         if (reviewService.updateReview(review)) {
             return ResponseEntity.noContent().build();
-<<<<<<< HEAD
+
         }else {
             return new ResponseEntity("Error", HttpStatus.CONFLICT);
-=======
-        } else {
-            return  ResponseEntity.status( HttpStatus.CONFLICT).build();
->>>>>>> fc0265dccf440bbb352a970c604edee58f2bb6dc
         }
     }
 
