ec71fd306ff27ce4b4719eb8987995361a86b54b
Rodriguez Morales,Noelia N <n.rodriguezmorales@student.fontys.nl>
2022-04-04 19:34:28 +0200

    Structure react with linked menu

--- gps-react-app/src/App.js
+++ gps-react-app/src/App.js
@@ -1,17 +1,19 @@
-//import Add_Application from "./Product/Add_Application";
-import React from "react";
-import Login from "./pages/Login"
-import Contact from "./pages/contact"
-import Home from "./pages/home"
-import About from "./pages/about"
-
 import { Routes, Route } from "react-router-dom";
-
-import './App.css';
-
+//Components
 import Header from './components/Header'
 import Container from './components/Container';
 import Footer from './components/Footer';
+
+//Pages
+import React from "react";
+import Login from "./pages/Login"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import About from "./pages/About"
+//import Add_Application from "./Product/Add_Application";
+
+//Css
+import './App.css';
 
 
 //APP STRUCTURE
@@ -24,9 +26,9 @@ function App() {
    <Container/>
    <Routes>
            <Route path="/Login" element={<Login/>} />
-           <Route path="/about" element={<About/>} />
-           <Route path="/contact" element={<Contact/>} />
-           <Route path="/home" element={<Home/>} />
+           <Route path="/About" element={<About/>} />
+           <Route path="/Contact" element={<Contact/>} />
+           <Route path="/Home" element={<Home/>} />
       </Routes>
    <Footer/>
   </>
