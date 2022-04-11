8909e0d58a239679d146b8f9a5e84d12ccdeef97
estherwolfs <estherwolfs@hotmail.com>
2022-04-11 09:01:33 +0200

    changes

--- GroupProjectApi/src/main/java/s3_gps_ivanti/DTO/ApplicationDetailedInfoDTO.java
+++ GroupProjectApi/src/main/java/s3_gps_ivanti/DTO/ApplicationDetailedInfoDTO.java
@@ -1,7 +1,7 @@
 package s3_gps_ivanti.DTO;
 
 import lombok.Data;
-import s3_gps_ivanti.model.Review;
+//import s3_gps_ivanti.model.Review;
 
 import java.util.ArrayList;
 
@@ -13,17 +13,17 @@ public class ApplicationDetailedInfoDTO {
     private ArrayList<String> images;
     private int totalDownloads;
     private int avgRating;
-    private ArrayList<Review> reviews;
+//    private ArrayList<Review> reviews;
     private ArrayList<VersionDownloadDTO> versions;
 
-    public ApplicationDetailedInfoDTO(String name, String description, ArrayList<String> images, int totalDownloads, int avgRating, ArrayList<Review> reviews,ArrayList<VersionDownloadDTO> versions)
+    public ApplicationDetailedInfoDTO(String name, String description, ArrayList<String> images, int totalDownloads, int avgRating,ArrayList<VersionDownloadDTO> versions)
     {
         this.name = name;
         this.description = description;
         this.images = images;
         this.totalDownloads = totalDownloads;
         this.avgRating = avgRating;
-        this.reviews = reviews;
+//        this.reviews = reviews;
         this.versions = versions;
     }
 }
