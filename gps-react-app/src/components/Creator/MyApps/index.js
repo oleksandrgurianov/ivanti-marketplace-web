da5d6d64fe3c2f998c7688d9c3880b19276b56f3
estherwolfs <estherwolfs@hotmail.com>
2022-04-11 10:56:49 +0200

    creator my apps react, no css

---
+++ gps-react-app/src/components/Creator/MyApps/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApplicationList from './ApplicationList'
+
+function MyApps() {
+  return (
+    <>
+        <ApplicationList />
+    </>
+  )
+}
+
+export default MyApps
