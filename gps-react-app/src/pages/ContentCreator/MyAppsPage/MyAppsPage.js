43e3bd4bdcc6c72ab957b7971b1dc3a6f7b6fcb9
estherwolfs <estherwolfs@hotmail.com>
2022-06-22 14:27:50 +0200

    add tutorial to my apps, detailed apps and add app page

--- gps-react-app/src/pages/ContentCreator/MyAppsPage/MyAppsPage.js
+++ gps-react-app/src/pages/ContentCreator/MyAppsPage/MyAppsPage.js
@@ -7,6 +7,8 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import loading from "../../../images/loading.gif";
 import useAuth from '../../../hooks/useAuth';
 import BasicApplication from '../../../components/BasicApplication';
+import Tutorial from '../../../components/Tutorial';
+import { BsQuestionCircle } from 'react-icons/bs'
 
 const MyAppsPage = () => {
 
@@ -32,14 +34,28 @@ const MyAppsPage = () => {
         getApplicationsByCreator()
     }, [])
 
+    const [showTutorial, setShowTutorial] = useState(false) 
+    const closeTutorial = () => {
+        setShowTutorial(false)
+    }
+    const tutorialContent = {
+        'title': 'My Applications Page',
+        'header': 'Manage your applications',
+        'body': 'On this page you will find an overview of all applications you have added. If you want to update or discontinue an application you can click on it to go to the application page, where you will find the controls. If you want to upload  a new application you can click the plus button.'
+    }
+
     return (
         <>
             <div className='my-apps'>
+            <Tutorial visible={showTutorial} closeTutorial={closeTutorial} content={tutorialContent}/>
                 <div className='my-apps-controls' style={{justifyContent: "normal"}}>
                     <h1 className='title'>My Apps</h1>
                     <Link to='/creator/add-app'><FontAwesomeIcon className='add-icon' icon={faCirclePlus}/></Link>
+                    <button onClick={() => setShowTutorial(true)} className='btn-open-tutorial'><BsQuestionCircle /></button>
+                    
                 </div>
                 <hr/>
+
                 {applications.length !== 0 ? (
                     <div className='my-apps-list'>
                         {applications?.map((app) => (
